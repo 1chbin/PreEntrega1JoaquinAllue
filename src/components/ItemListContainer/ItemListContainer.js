334b20c7a5fd8aa2ac5dcf94ce92618aa3d5ffe1
Joaquin-Allue <Alluejoaquin@hotmail.com>
2023-05-30 22:21:51 -0300

    primera pre entrega

---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,10 @@
+const ItemListContainer = ({greeting}) => {
+    return (
+        <div className="container2">
+            <h1 className="texto">{greeting}</h1>
+            <h2 className="subtitulo">En este podremos ver mi avance en ReactJS</h2>
+        </div>
+    )
+}
+
+export default ItemListContainer
